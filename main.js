6250eb9c587f5975641555cfbb94457fa687afc3
Codelesshermit <anto100charles@gmail.com>
2022-01-01 08:49:04 +0300

    solved the click on image alt bug

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ let image = document.querySelectorAll(".image");
 image.forEach((image)=>{
   image.addEventListener('click', (e)=>{
   console.log(e);
- 
-  e.preventDefault();
-  let imageAlt = document.querySelectorAll(".image").getAttribute('alt');
 
-alert(`you clicked on Image named ${imageAlt}`);
+  e.preventDefault();
+
+
+     let displayName = image.getAttribute('alt');
+
+  
+
+alert(`you clicked on Image named ${displayName}`);
 }) }
 );
