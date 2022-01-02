fc3b299a069ebc618cfb910062359dd817bb36f1
Codelesshermit <anto100charles@gmail.com>
2022-01-02 11:44:41 +0300

    developed a modal to help withimage downloading

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 
 let image = document.querySelectorAll(".image");
+let modal = document.querySelector(".modal");
+let modalContent = document.querySelector(".modal-body");
+let closeBtn = document.querySelector(".close");
+
+
 
 
 image.forEach((image)=>{
@@ -8,11 +13,21 @@ image.forEach((image)=>{
 
   e.preventDefault();
 
+ modal.style.display = "block";
 
-     let displayName = image.getAttribute('alt');
+let imageLink = image.getAttribute('src');
+document.querySelector(".modal-body a").setAttribute("href", imageLink);
 
+let displayName = image.getAttribute('alt');
+let title = document.querySelector(".title");
+title.append(displayName);
   
+ closeBtn.addEventListener( 'click', function(){  
+  modal.style.display = "none";
 
-alert(`you clicked on Image named ${displayName}`);
+  image.removeEventListener('click', e);
+});
+//alert(`you clicked on Image named ${displayName}`);
 }) }
 );
+
